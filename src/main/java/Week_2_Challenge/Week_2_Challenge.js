a00fd1816657c881587042f603c5b2dcec167a6d
mikezanger <michaelkzanger@gmail.com>
2023-07-10 20:08:41 -0500

    Area of Triangle

---
+++ src/main/java/Week_2_Challenge/Week_2_Challenge.js
@@ -0,0 +1,10 @@
+
+function FirstReverse(str) {
+        var splitString = str.split("")
+        var reverseArray = splitString.reverse()
+        var joinArray = reverseArray.join("")
+
+        return joinArray;
+        }
+
+        console.log(FirstReverse("hello"));
